408e5a74486222f12fd39e2a1373d70f39c1ed34
NishantKumarTiwari <nishant@italentindia.com>
2022-02-03 01:45:49 -0800

    fixed manu design issues

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 const preLoad = document.querySelector("#loading");
 const sideBar  = document.querySelector(".sidebar") 
+const Input_validate = document.querySelector(".ip-validate") 
+const val = document.querySelector(".val") 
 const preLoader=()=> {
     setTimeout(()=>{
         preLoad.style.display = 'none'
@@ -8,4 +10,13 @@ const preLoader=()=> {
 }
 const sideshow = () =>{
     sideBar.classList.toggle('active')
+}
+const inputValidate=()=>{
+    data = Input_validate.value
+    console.log(data)
+    if(data.length < 6 ){
+        val.innerHTML = "*Less "
+    }else{
+        val.innerHTML = ""
+    }
 }
